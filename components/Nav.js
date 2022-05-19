ed3efe9b1241318a2efcd20347632ebc82032800
dreinsteincode <santiago239@gmail.com>
2022-05-19 17:08:12 -0600

    Main NavBar

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,21 @@
-import Image from "next/image"
-import logo from "../images/logo-nav.png"
 
 export default function Nav() {
   return (
     <div className="nav">
-      <h1>Nav Component</h1>
+    <img src="https://user-images.githubusercontent.com/97049247/169415230-3b482532-4730-4985-8a67-9b8746ebdc80.png"/>
+      <nav>
+        <ul className="nav--list">
+
+          <li className="nav--item"><a>Home</a></li>
+          <li className="nav--item"><a>About Us</a></li>
+          <li className="nav--item"><a>Shop</a></li>
+          <li className="nav--item"><a>Donate</a></li>
+          <li className="nav--item"><a>Become A Volunteer</a></li>
+          <li className="nav--item"><a>Contact</a></li>
+
+
+        </ul>
+      </nav>
     </div>
   )
 }
