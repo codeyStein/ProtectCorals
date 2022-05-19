b52c44446bb1aa39674c222879bfd893dd17a6fb
dreinsteincode <santiago239@gmail.com>
2022-05-18 20:35:36 -0600

    more meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -12,17 +12,17 @@ function MyApp({ Component, pageProps }) {
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:url" content="https://protectcorals.netlify.app" />
       <meta property="og:title" content="Protect Corals" />
       <meta property="og:description" content="Make 2022 the year you make a difference" />
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="og:image" content="https://protectcorals.netlify.appassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:url" content="https://protectcorals.netlify.app" />
       <meta property="twitter:title" content="Protect Corals" />
       <meta property="twitter:description" content="Make 2022 the year you make a difference" />
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:image" content="https://protectcorals.netlify.appassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
     </Head>
   <h1> App </h1>
     </div>
