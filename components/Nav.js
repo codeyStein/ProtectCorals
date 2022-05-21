e19c039a678992ce57d4ca8ebfe2225b6c80da62
codeyStein <san.26.2010@icloud.com>
2022-05-20 18:20:04 -0600

    Fixed Classes In Nav Ellement

--- components/Nav.js
+++ components/Nav.js
@@ -2,33 +2,33 @@
 export default function Nav() {
   return (
     <div className="nav">
-    <nav>
-    <ul class="nav--list">
-      <li class="nav--item">
-        <a href="#">
-          <img src="https://github.com/codeyStein/ProtectCorals/blob/main/images/logo-nav.png?raw=true"/>
-        </a>
-      </li>
-      <li class="nav--item">
-        <a href="#">Home</a>
-      </li>
-      <li class="nav--item">
-        <a href="#">About Us</a>
-      </li>
-      <li class="nav--item">
-        <a href="#">Shop</a>
-      </li>
-      <li class="nav--item">
-        <a href="#">Donate</a>
-      </li>
-      <li class="nav--item">
-        <a href="#">Join Us</a>
-      </li>
-      <li class="nav--item">
-        <a href="#">Contact</a>
-      </li>
-    </ul>
-  </nav>
-    </div>
+      <nav>
+      <ul className="nav--list">
+        <li className="nav--item">
+          <a href="#">
+            <img src="https://github.com/codeyStein/ProtectCorals/blob/main/images/logo-nav.png?raw=true" alt="ProtectCorals Navbar Logo"/>
+          </a>
+        </li>
+        <li className="nav--item">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav--item">
+          <a href="#">About Us</a>
+        </li>
+        <li className="nav--item">
+          <a href="#">Shop</a>
+        </li>
+        <li className="nav--item">
+          <a href="#">Donate</a>
+        </li>
+        <li className="nav--item">
+          <a href="#">Join Us</a>
+        </li>
+        <li className="nav--item">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <Head>
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
 
       {/* Primary Meta Tags */}
