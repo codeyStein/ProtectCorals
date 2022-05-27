f64cafd246a8b99850571b26b2040add7088ffd2
codeyStein <san.26.2010@icloud.com>
2022-05-27 15:37:06 -0600

    Better Info BUtton Stylings

--- components/Info.js
+++ components/Info.js
@@ -1,13 +1,20 @@
 import React from "react"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleInfo
 } from "@fortawesome/free-solid-svg-icons";
 
+import Modal from "./Modal"
+
 export default function Info() {
   return (
     <div className="info">
-     <button className="info" onClick={console.log('clicked')}> <FontAwesomeIcon icon={faCircleInfo} style={{ fontSize: 40}}/> </button>
+     <a className="info" onClick={console.log('clicked')}> <FontAwesomeIcon icon={faCircleInfo} style={
+       {
+          fontSize: 45,
+        }
+       }/> </a>
     </div>
   )
 }
