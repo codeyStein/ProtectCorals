9060f5a216bf3ec2ded08839f4f3bfea23487417
codeyStein <san.26.2010@icloud.com>
2022-06-17 09:36:45 -0600

    About hero section done

--- components/About.js
+++ components/About.js
@@ -8,7 +8,7 @@ export default function About() {
 		<div className="About">
 			<img src={wave} alt="about section divder" className="about--wave" draggable={false}/>
 			<div className="about--container">
-				<h2>About Section!</h2>
+				<h2>About</h2>
 				<p>
 					We are a group of high school student on a mission to help mitigate our planet’s climate crisis, as well as aid communities which are in need of resources.....
 				</p>
