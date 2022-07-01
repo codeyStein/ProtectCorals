edfe7123d8ed1a192dd6349940187fb77e06d7ea
codeyStein <san.26.2010@icloud.com>
2022-07-01 14:59:20 -0600

    Basic button component styling

--- components/Button.js
+++ components/Button.js
@@ -4,6 +4,11 @@ export default function Button(props) {
 
 
 const style = {
+cursor: "pointer",
+padding: "20px 50px",
+borderRadius: "15px",
+fontSize: "1.25rem",
+fontFamily:"poppins",
 background: props.outline ? "transparent"
 					  : props.color==="blue" ? "#3B66CE" 
 						: "#fff",
@@ -11,9 +16,9 @@ border: props.color==="white" && props.outline ? "1px solid #fff"
 				 : props.color==="blue" && props.outline ? "1px solid #3B66CE" 
 			   : "none",
 color: props.color==="white" && props.outline ? "#fff"
-: props.color==="blue" && props.outline ? "#3B66CE"
-: props.color==="blue" && props.outline===false ? "#fff"
-: "#3B66CE" 
+			 : props.color==="blue" && props.outline ? "#3B66CE"
+			 : props.color==="blue" && props.outline===false ? "#fff"
+			 : "#3B66CE"
 
 }
 
